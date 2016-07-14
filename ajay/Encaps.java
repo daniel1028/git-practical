8893f485afec812a9be68bf1b73c7f4dbd6c0fbb
Ajaykumar Srinivasan <ajaykumar.s@bambeeq.com>
2016-07-14 11:50:30 +0530

    Requested by ajay

--- ajay/Encaps.java
+++ ajay/Encaps.java
@@ -1,11 +1,11 @@
 public class Encaps{
     public static void main(String args[]){
          Encapsulation obj = new Encapsulation();
-         obj.setEmpName("Mario");
-         obj.setEmpAge(32);
-         obj.setEmpSSN(112233);
+         obj.setEmpName("Ajay");
+         obj.setEmpAge(18);
+         obj.setId(112233);
          System.out.println("Employee Name: " + obj.getEmpName());
-         System.out.println("Employee SSN: " + obj.getEmpSSN());
+         System.out.println("Employee SSN: " + obj.getId());
          System.out.println("Employee Age: " + obj.getEmpAge());
     } 
 
