a9e48ad776f02458f5dc0a1b6d3ccb1bc4ff5c8a
Ajaykumar Srinivasan <ajaykumar.s@bambeeq.com>
2016-07-14 15:24:31 +0530

    move all the tasks inside Task folder

---
+++ ajay/Task/src/Encapsulation.java
@@ -0,0 +1,32 @@
+public class Encapsulation {
+
+	// TODO Auto-generated method stub
+	private int id;
+	private String empName;
+	private int empAge;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpAge() {
+		return empAge;
+	}
+
+	public void setEmpAge(int empAge) {
+		this.empAge = empAge;
+	}
+
+}
