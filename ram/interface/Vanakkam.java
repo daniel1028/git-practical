6968866c49c3c1ec26494790ebce556f7e9654da
Ramkumar <RamkumarSiranjeevi>
2016-07-14 13:03:05 +0530

    Committed by Ramkumar

---
+++ ram/interface/Vanakkam.java
@@ -0,0 +1,17 @@
+public class Vanakkam implements Printable, Showable {
+
+	public void print() {
+		System.out.println("Hello");
+	}
+
+	public void show() {
+		System.out.println("Welcome");
+	}
+
+	public static void main(String args[]) {
+		Vanakkam obj = new Vanakkam();
+		obj.print();
+		obj.show();
+	}
+
+}
---
+++ ram/method overriding/Interest.java
@@ -0,0 +1,10 @@
+public class Interest {
+	public static void main(String args[]) {
+		SBI s = new SBI();
+		ICICI i = new ICICI();
+		Axis a = new Axis();
+		System.out.println("SBI Rate of Interest: " + s.getRateOfInterest());
+		System.out.println("ICICI Rate of Interest: " + i.getRateOfInterest());
+		System.out.println("AXIS Rate of Interest: " + a.getRateOfInterest());
+	}
+}
