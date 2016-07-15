775ac0105ccb17f07c1b9b6df9ff25f4af7bda79
Vijayaraman Panneerselvam <vijayaraman.p@bambeeq.com>
2016-07-15 10:56:17 +0530

    Task by vijay

--- vijay/ImplementOverloding.java
+++ vijay/ImplementOverloding.java
@@ -1,26 +1,34 @@
+//Implement method overloading
 package applicationclass;
 
 public class ImplementOverloding {
-	 void data (int a)
-	    {
-	       System.out.println ("Values of datas a : " + a);
-	    }
-	    void data (int a, int b)
-	    {
-	       System.out.println ("values of datas bvgvsdg a and b: " + a + "," + b);
-	    }
-	    double data(double a) {
-	       System.out.println("double a: " + a);
-	       return a*a;
-	    }
+	void data(int a) {
+		System.out.println("Values of data a : " + a);
+	}
+
+	void data(int a, int b) {
+		System.out.println("values of data a and b: " + a + "," + b);
+	}
+
+	double data(double a) {
+		System.out.println("double a: " + a);
+		return a * a;
+	}
+
 	public static void main(String[] args) {
 		ImplementOverloding Object = new ImplementOverloding();
-	        double result;
-	        Object .data(10);
-	        Object .data(10, 20);
-	        result = Object .data(5.5);
-	        System.out.println("Data output : " + result);
+		double result;
+		Object.data(10);
+		Object.data(10, 20);
+		result = Object.data(5.5);
+		System.out.println("Data output : " + result);
 
 	}
 }
+/*
+ * output
+ * 
+ * Values of data a : 10 values of data a and b: 10,20 double a: 5.5 Data output
+ * : 30.25
+ */
 
